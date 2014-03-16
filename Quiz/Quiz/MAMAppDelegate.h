7ae8e334373d701961cd3b2a8a54a4dbf303d9f5
Miguel Angel Moreno <miguel.s.address@gmail.com>
2014-03-16 18:56:55 +0100

    Lesson 1: Quiz Application first draft. It has 3 questions and their 3 answers and you can tap the buttons to play in a cyclical way

---
+++ Quiz/Quiz/MAMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MAMAppDelegate.h
+//  Quiz
+//
+//  Created by MIGUEL ANGEL MORENO ARMENTEROS on 16/03/14.
+//  Copyright (c) 2014 MIGUEL ANGEL MORENO ARMENTEROS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MAMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quiz/Quiz/MAMAppDelegate.m
@@ -0,0 +1,55 @@
+//
+//  MAMAppDelegate.m
+//  Quiz
+//
+//  Created by MIGUEL ANGEL MORENO ARMENTEROS on 16/03/14.
+//  Copyright (c) 2014 MIGUEL ANGEL MORENO ARMENTEROS. All rights reserved.
+//
+
+#import "MAMAppDelegate.h"
+#import "MAMQuizViewController.h";
+
+@implementation MAMAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+    
+    //Assingning my MAMQuizViewController as root controller
+    MAMQuizViewController *quizVC = [[MAMQuizViewController alloc] init];
+    self.window.rootViewController = quizVC;
+    
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+@end
