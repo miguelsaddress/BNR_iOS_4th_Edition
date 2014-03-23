7524b6b987e3ae08169d9f9a86d306b7236f1cc1
Miguel Angel Moreno <miguel.s.address@gmail.com>
2014-03-23 17:55:09 +0100

    First step of the chapter, reacting to touch on the view to change color randomly

---
+++ 05/Hypnosister/Hypnosister/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Hypnosister
+//
+//  Created by Miguel Angel Moreno Armenteros on 21/03/14.
+//  Copyright (c) 2014 Miguel Angel Moreno Armenteros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
